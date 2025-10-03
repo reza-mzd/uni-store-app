d13a49f1f10d8e2f0540df37d7b06eb7268c586b
Reza-mohammadizadeh <rezamohammadizadeh19@gmail.com>
2025-10-03 20:14:49 +0330

    Add test view in store app to display user IP and sample products

--- store/views.py
+++ store/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from . import models
 # Create your views here.
+
+def hello_world(request):
+    ip = request.META.get('HTTP_X_FORWARDED_FOR')
+    
+    if ip:
+        ip = ip.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    return HttpResponse(f"Your IP Address is: {ip}")
+
+def show_products(request):
+    html = ''
+    products = models.Product.objects.all()
+    for product in products:
+        html += product.name + "<br/>"
+    
+    return HttpResponse(html) 
+
+        
+    
