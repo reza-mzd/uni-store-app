75d97a88833f8f60282bf5fd6d85b551734fcdb4
Reza-mohammadizadeh <rezamohammadizadeh19@gmail.com>
2025-10-21 01:20:04 +0330

    fix(urls): minor update in store app URL patterns

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ app_name = 'store'
 
 urlpatterns = [
     path('yourIP/', views.hello_world),
-    path('product/', views.show_products, name='product_list'),
+    path('product/', views.ProductListview.as_view(), name='product_list'),
 ]
