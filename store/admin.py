bb0945eabe454d577fa394392342f27b22e727ec
Reza-mohammadizadeh <rezamohammadizadeh19@gmail.com>
2025-10-21 01:13:19 +0330

    feat(admin): customize Product admin with list_display and comments count

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'discount', 'count', 'is_deleted', 'get_comments_count')
+    # readonly_fields = ['price']
+    def get_comments_count(self, obj: Product):
+        return obj.comments.all().count()
+    get_comments_count.short_description = 'Comments'
+    class Meta:
+        model = Product
+        
+admin.site.register(Product, ProductAdmin)  
