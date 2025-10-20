540deeb281c44c8ec292f9ae9e1b45581f5f7c74
Reza-mohammadizadeh <rezamohammadizadeh19@gmail.com>
2025-10-21 00:48:46 +0330

    chore(settings): configure STATIC and MEDIA paths for deployment

--- dj_uni/settings.py
+++ dj_uni/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dj_uni.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/WWW/html/staticfiles/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
