55e18fcf59dc8539a781cadc2bff2adea8f8357c
Reza-mohammadizadeh <rezamohammadizadeh19@gmail.com>
2025-10-30 18:57:26 +0330

    feat(store): Implement product image upload and pricing logic

--- store/models.py
+++ store/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+import uuid
+import os
+from datetime import datetime
+
+def _get_product_file_upload_path(instance, filename):
+    prefix = datetime.now().strftime('product_images/%Y%m')
+    ext = os.path.splitext(filename)[1]
+    new_name = f"{uuid.uuid4()!s}{ext}"
+    path = os.path.join(prefix, new_name)
+    return path
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -7,15 +17,26 @@ class Product(models.Model):
     discount = models.PositiveIntegerField(default=0)
     description = models.TextField(null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
-    picture = models.ImageField(upload_to='products_images/', null=True, blank=True)
+    picture = models.ImageField(upload_to=_get_product_file_upload_path, null=True, blank=True)
+    #comments
+    
+    def calculate_discounted_prices(self):
+        res = int(self.price - (self.price * self.discount / 100))
+        if res < 0:
+            raise ValueError("Bad discount value")
+        else:
+            return res
     
     def __str__(self):
         return self.name
-    
-    
+
+# from django.contrib.auth.models import User   # explain : Instead of using the User class directly, use the following two lines
+from django.contrib.auth import get_user_model
+User = get_user_model()
     
 class Comment(models.Model):
-        date = models.DateTimeField(auto_now_add=True)
-        body = models.TextField()
-        product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='comments')
-        reply_to = models.ForeignKey('self', on_delete=models.PROTECT, null=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='comments')
+    date = models.DateTimeField(auto_now_add=True) 
+    body = models.TextField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='comments')
+    reply_to = models.ForeignKey('self', on_delete=models.PROTECT, null=True)
